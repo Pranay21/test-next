96a604b4bcc5d21c800c76939c4cbbeeee79dceb
Pranaya Bhat <prbhat@qca-sdc.com>
2018-10-12 13:13:18 -0500

    Updates

---
+++ precache-manifest.5549e8f73037db9225fe4e00d1f1deaa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "428f5f241642682b5ae6",
+    "url": "/test-next/static/js/runtime~main.428f5f24.js"
+  },
+  {
+    "revision": "4a2ca08936868609eb2c",
+    "url": "/test-next/static/js/main.4a2ca089.chunk.js"
+  },
+  {
+    "revision": "9466fa0fd97a3c19a2e9",
+    "url": "/test-next/static/js/1.9466fa0f.chunk.js"
+  },
+  {
+    "revision": "9e245dbb3459816c779de7687d8b2f39",
+    "url": "/test-next/index.html"
+  }
+];
